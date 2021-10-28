604d17ae3821a9178efc2c5b1cdec96c5134ecc8
rasyidev <habib.rasyid11@gmail.com>
2021-10-28 15:55:55 +0700

    Membuat CS Restoran Sederhana

--- main.py
+++ main.py
@@ -21,18 +21,62 @@ def bilang(text):
     ps.playsound(filename)
   except:
      print('Maaf lagi sariawan, gabisa ngomong dulu')
+     print(text)
+
+
+pesanan = {
+  'pemesan': "",
+  'makanan': ""
+}
+
+def tampil_menu():
+  print("1. Bakso Bakar ")
+  print("2. Ayam Geprek sedap")
+  print("3. Pepes Ikan Kerapu")
+  text = "Mau Pesan apa?. Kami sedia Bakso Bakar, Ayam Geprek Sedap dan Pepes Ikan Kerapu"
+  bilang(text)
+  makanan = dengerin()
+  if makanan == "Bakso Bakar":
+    pesanan['makanan'] 
+    bilang("Oke, pesan bakso bakar yaa")
+  tanya_nama("Atas Nama siapa?")
+  bilang(f"Oke {pesanan['nama'],} dengan pesanan {pesanan['makanan']} yaa. Mohon ditunggu yaa")
+
+def tanya_nama(text):
+  tts = gTTS(text=text, lang="id")
+  filename = "tanya_nama.mp3"
+  print(text)
+  tts.save(filename)
+  try:
+    ps.playsound(filename)
+  except:
+     print('Maaf lagi sariawan, gabisa ngomong dulu')
+
+  nama = dengerin()
+  pesanan['pemesan'] = nama
+
+
+# def pesan_makanan():
+#   tampil_menu()
+#   makanan = dengerin()
+#   if makanan == "Bakso Bakar":
+#     print("Bakso Bakar")
+
+  
+
+
+
 
 while True:
   try:
     text = dengerin()
-    bilang(text)
-     
-
     if text in recognize_exit:
       text = 'Makasih udah pake Voice Recognition Bahasa Indonesia!, Sampai Jumpa!'
       print(text)
       bilang(text)
       break
+    else:
+      tampil_menu()
   except:
     recognizer = speech_recognition.Recognizer()
     continue
